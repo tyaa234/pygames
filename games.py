8ea592d1f9ba476dd61a79e6d5b43229a47863f3
tyaa234 <mutianugrah23@gmail.com>
2024-03-18 22:10:55 +0800

    first commit

---
+++ games.py
@@ -0,0 +1,50 @@
+def main():
+    print("Pilih Salah Satu!")
+    print("1. Kertas")
+    print("2. Batu")
+    print("3. Gunting")
+    pilih = int(input("Masukkan Pilihan Anda: "))
+    import random
+    comp = random.randint(1,3)
+    if pilih == 1:
+        if comp == 1:
+            print("Kamu Memilih Kertas")
+            print("Komputer Memilih Kertas")
+            print("Seri!")
+        elif comp == 2:
+            print("Kamu Memilih Kertas")
+            print("Komputer Memilih Batu")
+            print("Komputer Menang!")
+        else:
+            print("Kamu Memilih Kertas")
+            print("Komputer Memilih Gunting")
+            print("Kamu Menang!")
+    elif pilih == 2:
+        if comp == 1:
+            print("Kamu Memilih Batu")
+            print("Komputer Memilih Kertas")
+            print("Kamu Menang!")
+        elif comp == 2:
+            print("Kamu Memilih Batu")
+            print("Komputer Memilih Batu")
+            print("Seri!")
+        else:
+            print("Kamu Memilih Batu")
+            print("Komputer Memilih Gunting")    
+            print("Komputer Menang!")
+    elif pilih == 3:
+        if comp == 1:
+            print("Kamu Memilih Gunting")
+            print("Komputer Memilih Kertas")
+            print("Komputer Menang!")
+        elif comp == 2:
+            print("Kamu Memilih Gunting")
+            print("Komputer Memilih Batu")
+            print("Kamu Menang!")
+        else:
+            print("Kamu Memilih Gunting")
+            print("Komputer Memilih Gunting")
+            print("Seri!")
+    else:
+        print("Pilihan Tidak Tersedia")
+main()
